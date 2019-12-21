7a6dbc07f3996d056682f6ce98530cd93ea63662
yudaikomori <yudai_komori@voyagegroup.com>
2019-12-21 16:51:42 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+
+```
+curl -fsSL https://raw.githubusercontent.com/yudaikomori/dotfiles/master/startup.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew install git
+brew install git-secrets
+brew install coreutils
+brew install awscli
+brew install openssl
+brew install fish
+brew install go
+brew install php
+brew install rbenv
+brew install ruby
+brew install perl
+brew install pyenv
+brew install python
+brew install node
+brew install direnv
+brew install binutils
+brew install findutils
+brew install gawk
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-which
+brew install grep
+brew install protobuf
+brew install jq
+brew install nkf
+brew install ghq
+brew install peco
+
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install docker
+brew cask install slack
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install jetbrains-toolbox
+
+ln -snf ~/dotfiles/files/.config ~/.config
+ln -snf ~/dotfiles/files/.editorconfig ~/.editorconfig
+ln -snf ~/dotfiles/files/.gitignore ~/.gitignore
+ln -snf ~/dotfiles/files/.gitconfig ~/.gitconfig
